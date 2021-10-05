0829bfe9e363ea7d6cf1568b2ab878dcef8fe176
Attila Varga2 <attila_varga2@epam.com>
2021-10-05 11:44:40 +0200

    added HigherOrderFunctions solution

--- src/basic/HigherOrderFunctions.java
+++ src/basic/HigherOrderFunctions.java
@@ -1,22 +1,33 @@
 package basic;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@SuppressWarnings({"Convert2Lambda", "Convert2Diamond"})
 public class HigherOrderFunctions {
-    private final Basic basic;
 
-    public HigherOrderFunctions(Basic basic) {
-        this.basic = basic;
+    public HigherOrderFunctions() {
     }
 
     public static void main(String[] args) {
-        HigherOrderFunctions higherOrderFunctions = new HigherOrderFunctions(new Basic());
-        higherOrderFunctions.printFirstTenOddNumber();
+        HigherOrderFunctions higherOrderFunctions = new HigherOrderFunctions();
+        higherOrderFunctions.printFirstTenNumbersByFilter(new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer input) {
+                return input % 2 != 1;
+            }
+        });
+        higherOrderFunctions.printFirstTenNumbersByFilter((Integer input) -> input % 2 == 1);
+        higherOrderFunctions.printFirstTenNumbersByFilter(input -> true);
     }
 
-    public void printFirstTenOddNumber() {
-        IntStream.range(0, 10)
-                .filter(basic::isOdd1)
-                .forEach(System.out::println);
+    public void printFirstTenNumbersByFilter(Predicate<Integer> filter) {
+        List<Integer> numbers = IntStream.range(0, 10)
+                .boxed()
+                .filter(filter)
+                .collect(Collectors.toList());
+        System.out.println("numbers = " + numbers);
     }
 }
