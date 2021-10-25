f88329b394cd252d64e34fef96dd97b41faee529
Attila Varga2 <attila_varga2@epam.com>
2021-10-25 14:28:16 +0200

    added getBookPricesByISBN

--- src/main/java/advanced/DefaultBookService.java
+++ src/main/java/advanced/DefaultBookService.java
@@ -5,18 +5,11 @@ import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static advanced.Type.KID;
 import static advanced.Type.NEW_RELEASE;
 import static advanced.Type.OLD;
-import static advanced.util.BigDecimalAverageCollector.averagingBigDecimal;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
 public class DefaultBookService implements BookService {
     private final DataProvider dataProvider;
@@ -27,74 +20,7 @@ public class DefaultBookService implements BookService {
         this.dataProvider = dataProvider;
     }
 
-    @Override
-    public List<Book> getAllBooks() {
-        return dataProvider.getAllBooks();
-    }
 
-    @Override
-    public List<Book> getAllBooksForAuthor(final Author author) {
-        return dataProvider.getAllBooks().stream()
-                .filter(author::isAuthorOf)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Book> getAllBooksSortedByTitleAscending() {
-        return dataProvider.getAllBooks().stream()
-                .sorted(bookTitleAscending)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Book> getAllBookSortedByTitleDescending() {
-        return dataProvider.getAllBooks().stream()
-                .sorted(bookTitleDescending)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Book> getDiscountedBooks() {
-        return dataProvider.getAllBooks().stream()
-                .map(book -> book.bookWithPrice(calculateDiscount(book)))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Long getCountOfBooksWithFirstLetter(final String letter) {
-        return dataProvider.getAllBooks().stream()
-                .map(Book::getTitle)
-                .filter(title -> title.startsWith(letter))
-                .count();
-    }
-
-    @Override
-    public String getLongestSubtitle() {
-        return dataProvider.getAllBooks().stream()
-                .map(Book::getSubTitle)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .max(Comparator.comparingInt(String::length))
-                .orElseThrow();
-    }
-
-    @Override
-    public Map<Character, Set<String>> getBookTitleByFirstLetter() {
-        return dataProvider.getAllBooks().stream()
-                .collect(Collectors.groupingBy(book -> book.getTitle().charAt(0), mapping(Book::getTitle, toSet())));
-    }
-
-    @Override
-    public Map<Type, BigDecimal> getAveragePriceByBookType() {
-        return dataProvider.getAllBooks().stream()
-                .collect(groupingBy(Book::getType, mapping(Book::getPrice, averagingBigDecimal())));
-    }
-
-    @Override
-    public Map<String, BigDecimal> getBookPricesByISBN() {
-        return dataProvider.getAllBooks().stream()
-                .collect(toMap(Book::getIsbn, Book::getPrice));
-    }
 
     private BigDecimal calculateDiscount(Book book) {
         final BigDecimal price = book.getPrice();
@@ -105,4 +31,53 @@ public class DefaultBookService implements BookService {
         };
     }
 
+    @Override
+    public List<Book> getAllBooksForAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBooksSortedByTitleAscending() {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBookSortedByTitleDescending() {
+        return null;
+    }
+
+    @Override
+    public List<Book> getDiscountedBooks() {
+        return null;
+    }
+
+    @Override
+    public Long getCountOfBooksWithFirstLetter(String letter) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public String getLongestSubtitle() {
+        return null;
+    }
+
+    @Override
+    public Map<Character, Set<String>> getBookTitleByFirstLetter() {
+        return null;
+    }
+
+    @Override
+    public Map<Type, BigDecimal> getAveragePriceByBookType() {
+        return null;
+    }
+
+    @Override
+    public Map<String, BigDecimal> getBookPricesByISBN() {
+        return null;
+    }
 }
